7299acd056668bb86810843100c6d7d98f8d1fb4
Dinuka Navaratna <Dinuka.navaratna@gmail.com>
2022-04-10 14:57:52 +0530

    2022.04.10

--- server/src/audio.py
+++ server/src/audio.py
@@ -83,7 +83,7 @@ class upload_audio(Resource):
             elif("Hard" in level):
                 accuracy = compare(filePath, "src/Recordings/"+context+".wav")
 
-            insert_q("query", user_id, "dyslexia", int(accuracy/10))
+            insert_q("query", user_id, "dyslexia", int(float(accuracy))/10)
 
             logger.debug("Accuracy: "+str(accuracy))
             return jsonify({"success":"true", "message":"{\"accuracy\":\""+str(accuracy)+"\"}"})
